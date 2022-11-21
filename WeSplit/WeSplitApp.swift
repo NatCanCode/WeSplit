e3c40f5c67c3558df6efb0eb9d2b7fb168b8c210
NatCanCode <nathalie.piat1@gmail.com>
2022-11-21 18:50:17 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by N N on 21/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
